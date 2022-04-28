3a8bae1c14c14d24301747507021cb281da37aac
DaviChavesPinheiro <davichavespinheiro011010@gmail.com>
2022-04-28 12:38:59 -0300

    [Duplicacao do diretorio]: inicio da duplicacao do diretorio + buckets

--- IHash.py
+++ IHash.py
@@ -8,6 +8,10 @@ class IHash:
         # Cria uma pasta para guardar os buckets (apenas para organizacão)
         if(not os.path.exists("./buckets")):
             os.mkdir("./buckets")
+        
+        # Deleta os buckets antigos
+        for i in os.listdir("./buckets"):
+            os.remove("./buckets/" + i)
 
         for i in range(2 ** profundidade_global):
             # No diretorio, será armazenado o indice "binário" + a profundidade local
@@ -27,13 +31,47 @@ class IHash:
         with open("./buckets/{}.txt".format(posicao_diretorio), 'r') as f:
             lines = f.readlines()
             print(lines)
-
-        if(len(lines) < 32):
+        
+        # TODO: MUDAR PARA 32
+        if(len(lines) < 3):
             # Adiciona uma entrada e escreve no disco
             lines.append("{},{}\n".format(ano, referencia))
             with open("./buckets/{}.txt".format(posicao_diretorio), 'w') as f:
                 f.writelines(lines)
         else:
-            print("fazer split")
+            _, prf_atual = self.diretorio[posicao_diretorio]
+            if(prf_atual + 1 > self.profundidade_global):
+                self.duplicar_diretorio()
+            index_bucket, index_bucket_novo = self.duplicar_bucket(posicao_diretorio)
+
+            # Incrementa a profundidade local dos dois buckets
+            _, prf_atual = self.diretorio[index_bucket]
+            self.diretorio[index_bucket] = (index_bucket, prf_atual + 1)
+            self.diretorio[index_bucket_novo] = (index_bucket_novo, prf_atual + 1)
+            print(self.diretorio)
+    
+    # Duplica o diretorio atual        
+    def duplicar_diretorio(self):
+        for i in range(2 ** self.profundidade_global, 2 ** (self.profundidade_global + 1)):
+            self.diretorio.append((i, self.profundidade_global))
+        self.profundidade_global += 1
+    
+    # Duplica o bucket e altera a referencia do diretorio 
+    def duplicar_bucket(self, index_bucket):
+        # Index do bucket que vai ser duplicado (split)
+        index, prf_local = self.diretorio[index_bucket]
         
+        # Index do novo bucket
+        index_novo = index_bucket + 2 ** prf_local
+        index_novo, prf_local_novo = self.diretorio[index_novo]
         
+        # Cria o bucket novo
+        with open("./buckets/{}.txt".format(index_novo), 'w') as f:
+            pass
+        
+        # Retorna o index do bucket antigo e do novo
+        return (index, index_novo)
+
+
+
+
--- Main.py
+++ Main.py
@@ -2,4 +2,6 @@ from Database import Database
 db = Database("arquivo_de_dados.txt", 2)
 db.add_registro(0, "teste", 1980, "tinto")
 db.add_registro(0, "teste", 2000, "tinto")
+db.add_registro(0, "teste", 2004, "tinto")
+db.add_registro(0, "teste", 2008, "tinto")
 db.add_registro(1, "teste2", 2001, "branco")
